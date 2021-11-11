02677f4a1fa3178921b94433eae109aa69b6ee08
sugino <0jb4f61k3853v2f@gmail.com>
2021-11-11 20:15:40 +0900

    投稿機能

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from .models import Post
+from .forms import PostForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
@@ -8,3 +10,73 @@ class IndexView(View):
         return render(request, 'portfolio/index.html', {
             'post_data': post_data
         })
+
+class PostDetailView(View):
+    def get(self, request, *args, **kwargs):
+        post_data = Post.objects.get(id=self.kwargs['pk'])
+        return render(request, 'portfolio/post_detail.html', {
+            'post_data': post_data
+        })
+
+class CreatePostView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        form = PostForm(request.POST or None)
+        return render(request, 'portfolio/post_form.html', {
+            'form': form
+        })
+    def post(self, request, *args, **kwargs):
+        form = PostForm(request.POST or None)
+
+        if form.is_valid():
+            post_data = Post()
+            post_data.author = request.user
+            post_data.title = form.cleaned_data['title']
+            post_data.content = form.cleaned_data['content']
+            post_data.save()
+            return redirect('post_detail', post_data.id)
+
+        return render(request, 'portfolio/post_form.html', {
+            'form': form
+        })
+
+class PostEditView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        post_data = Post.objects.get(id=self.kwargs['pk'])
+        form = PostForm(
+            request.POST or None,
+            initial = {
+                'title': post_data.title,
+                'content': post_data.content
+            }
+        )
+
+        return render(request, 'portfolio/post_form.html', {
+            'form': form
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = PostForm(request.POST or None)
+
+        if form.is_valid():
+            post_data = Post.objects.get(id=self.kwargs['pk'])
+            post_data.author = request.user
+            post_data.title = form.cleaned_data['title']
+            post_data.content = form.cleaned_data['content']
+            post_data.save()
+            return redirect('post_detail', self.kwargs['pk'])
+
+        return render(request, 'portfolio/post_form.html', {
+            'form': form
+        })
+
+class PostDeleteView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        post_data = Post.objects.get(id=self.kwargs['pk'])
+        return render(request, 'portfolio/post_delete.html', {
+            'post_data': post_data
+        })
+
+    def post(self, request, *args, **kwargs):
+        post_data = Post.objects.get(id=self.kwargs['pk'])
+        post_data.delete()
+        return redirect('index')
