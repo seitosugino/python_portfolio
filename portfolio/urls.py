61c1d9f8bc24e845b30f516ece0bcf5570b89e6d
sugino <0jb4f61k3853v2f@gmail.com>
2021-11-16 02:15:02 +0900

    he

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,13 +3,13 @@ from portfolio import views
 from django.urls.conf import include
 
 urlpatterns = [
-    #path('', views.IndexView.as_view(), name='index'),
-    #path('post/<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
-    #path('post/new/', views.CreatePostView.as_view(), name='post_new'),
-    #path('post/<int:pk>/edit/', views.PostEditView.as_view(), name='post_edit'),
-    #path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
-    #path('category/<str:category>', views.CategoryView.as_view(), name='category'),
-    #path('search', views.SearchView.as_view(), name='search'),
-    #path('address/', views.AddressEditView.as_view(), name='address'),
-    #path('app/', include('app.urls')),
+    path('', views.IndexView.as_view(), name='index'),
+    path('post/<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.CreatePostView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/', views.PostEditView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+    path('category/<str:category>', views.CategoryView.as_view(), name='category'),
+    path('search', views.SearchView.as_view(), name='search'),
+    path('address/', views.AddressEditView.as_view(), name='address'),
+    path('app/', include('app.urls')),
 ]
