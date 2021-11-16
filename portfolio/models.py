e3d844ae35a1a8f9a7331ee81c7077db5049e602
sugino <0jb4f61k3853v2f@gmail.com>
2021-11-16 13:11:06 +0900

    heroku

--- portfolio/models.py
+++ portfolio/models.py
@@ -10,7 +10,7 @@ class Category(models.Model):
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, verbose_name='カテゴリ',on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, verbose_name='カテゴリ',on_delete=models.SET_NULL, null=True)
     title = models.CharField("タイトル", max_length=200)
     image = models.ImageField(upload_to='images', verbose_name='イメージ画像', null=True, blank=True)
     content = models.TextField("本文")
