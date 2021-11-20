6efae31496028e544c5a5e06b3680ecabf83c7b3
sugino <0jb4f61k3853v2f@gmail.com>
2021-11-21 01:46:29 +0900

    予約サイトデプロイ

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,13 +20,13 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
-    #path('', include('mypage.urls')),
-    #path('post/', include('portfolio.urls')),
-    #path('app/', include('app.urls')),
-    #path('accounts/', include('accounts.urls')),
-    #path('accounts/', include('allauth.urls')),
-    #path('reserve/', include('reserve.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('mypage.urls')),
+    path('post/', include('portfolio.urls')),
+    path('app/', include('app.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('reserve/', include('reserve.urls')),
 ]
 
 if settings.DEBUG:
